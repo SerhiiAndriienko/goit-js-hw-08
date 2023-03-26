ba354d4eb63184ba3a3d485a332746ae77b850fc
serhiiandriienko <116962959+SerhiiAndriienko@users.noreply.github.com>
2023-03-26 15:09:09 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -11,7 +11,7 @@ formEl.addEventListener('submit', clickOnSubmitBtn);
 messageEl.addEventListener('input', _.debounce(getMessageValue, 1000));
 inputEl.addEventListener('input', _.debounce(getInputValue, 1000));
 
-if (parseFormObject.email !== '' || parseFormObject.message !== '') {
+if (parseFormObject.email !== null || parseFormObject.message !== null) {
   inputEl.value = `${parseFormObject.email}`;
   messageEl.value = `${parseFormObject.message}`;
 }
