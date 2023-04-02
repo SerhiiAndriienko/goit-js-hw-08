c8f010e48e18c6833c69b000af3f53fb9fd1af09
serhiiandriienko <116962959+SerhiiAndriienko@users.noreply.github.com>
2023-04-02 16:59:31 +0200

    work

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,26 +1,40 @@
-import Player from '@vimeo/player';
-var _ = require('lodash');
+// import Player from '@vimeo/player';
+// var _ = require('lodash');
 
-const iframe = document.querySelector('iframe');
-const player = new Player(iframe);
-let videoStartTime = 0;
-const startVideo = localStorage.getItem('videoStart');
-if (startVideo) {
-  videoStartTime = JSON.parse(startVideo);
-}
-player.setCurrentTime(videoStartTime);
+// const iframe = document.querySelector('iframe');
+// const player = new Player(iframe);
+// let videoStartTime = 0;
+// const startVideo = localStorage.getItem('videoStart');
+// if (startVideo) {
+//   videoStartTime = JSON.parse(startVideo);
+// }
+// player.setCurrentTime(videoStartTime);
 
-player.on('play', onPlay);
-player.on('pause', onPause);
+// player.on('play', onPlay);
+// player.on('pause', onPause);
 
-function onPlay() {
-  console.log('played the video!');
-  player.on('timeupdate', _.throttle(setTime, 1000));
-}
-function onPause() {
-  console.log('pause the video!');
-  player.off('timeupdate');
-}
-function setTime({ seconds }) {
-  localStorage.setItem('videoStart', JSON.stringify(seconds));
-}
+// function onPlay() {
+//   console.log('played the video!');
+//   player.on('timeupdate', _.throttle(setTime, 1000));
+// }
+// function onPause() {
+//   console.log('pause the video!');
+//   player.off('timeupdate');
+// }
+// function setTime({ seconds }) {
+//   localStorage.setItem('videoStart', JSON.stringify(seconds));
+// }
+
+// Change code below this line
+const calculateTotalPrice = orderedItems => {
+  let totalPrice = 0;
+
+  orderedItems.forEach(item => {
+    totalPrice += item;
+    console.log(totalPrice);
+  });
+  console.log(totalPrice);
+};
+
+calculateTotalPrice([23, 9, 9, 9, 18]);
+// Change code above this line
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -7,33 +7,39 @@ let FORM_OBJECT = localStorage.getItem('feedback-form-state');
 let parseFormObject = JSON.parse(FORM_OBJECT);
 let inputValue = '';
 let messageValue = '';
+const key = 'feedback-form-state';
 formEl.addEventListener('submit', clickOnSubmitBtn);
 messageEl.addEventListener('input', _.throttle(getMessageValue, 500));
 inputEl.addEventListener('input', _.throttle(getInputValue, 500));
 
 if (FORM_OBJECT) {
+  console.log(parseFormObject.email);
+  console.log(parseFormObject.message);
+
   inputEl.value = `${parseFormObject.email}`;
   messageEl.value = `${parseFormObject.message}`;
 }
 
 function getInputValue(event) {
   inputValue = event.target.value;
+  messageValue = messageEl.value;
   localStorage.setItem(
-    'feedback-form-state',
+    key,
     JSON.stringify({ message: `${messageValue}`, email: `${inputValue}` })
   );
 }
 function getMessageValue(event) {
+  inputValue = inputEl.value;
   messageValue = event.target.value;
   localStorage.setItem(
-    'feedback-form-state',
+    key,
     JSON.stringify({ message: `${messageValue}`, email: `${inputValue}` })
   );
 }
 function clickOnSubmitBtn(event) {
   event.preventDefault();
-  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
-  localStorage.clear();
+  console.log(JSON.parse(localStorage.getItem(key)));
+  localStorage.removeItem(key);
 
   inputEl.value = '';
   messageEl.value = '';
