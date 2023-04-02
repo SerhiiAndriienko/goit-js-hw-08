a2fbc858d8192a6460f7f2274f6db0ac5ffa379d
SerhiiAndriienko <116962959+SerhiiAndriienko@users.noreply.github.com>
2023-04-02 15:00:08 +0000

    Deploying to gh-pages from @ SerhiiAndriienko/goit-js-hw-08@c8f010e48e18c6833c69b000af3f53fb9fd1af09 🚀

---
+++ 02-video.2755efd1.js
@@ -0,0 +1,2 @@
+!function(){var o;o=0,[23,9,9,9,18].forEach((function(n){o+=n,console.log(o)})),console.log(o)}();
+//# sourceMappingURL=02-video.2755efd1.js.map
---
+++ 02-video.a842f592.js
@@ -0,0 +1,2 @@
+(o=>{let l=0;o.forEach((o=>{l+=o,console.log(l)})),console.log(l)})([23,9,9,9,18]);
+//# sourceMappingURL=02-video.a842f592.js.map
